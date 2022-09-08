cdf511498133591eeeaf1659804b62d14ef21445
abisajit <89920012+abisajit@users.noreply.github.com>
2022-09-08 23:49:36 +0530

    Add files via upload

---
+++ musiclist.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+      name: "TOOFAN - KGF 2",
+      artist: "Deepak-blue",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "ARABIC KUTHU - BEAST",
+      artist: "Anirudh & Jonita Gandhi",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "OO SOLRIYA MAMA- PUSHPA",
+      artist: "DSP & Andrea",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "BAE - DON",
+      artist: "Anirudh",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "UNNALAE - RADHE SHYAM",
+      artist: "Shreya Ghosal",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "TWO TWO - KRK",
+      artist: "Anirudh",
+      img: "music-6",
+      src: "music-6"
+    },
+  ];
